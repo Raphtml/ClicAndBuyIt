ba653321cfd315b90755b51acf66c4f3f2dc6324
Raphael Liere <raphael.liere@hotmail.fr>
2021-04-23 22:46:14 +0200

    crud advert OK

---
+++ src/Form/AdvertType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Advert;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdvertType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('photo')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('user')
+            ->add('category')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Advert::class,
+        ]);
+    }
+}
