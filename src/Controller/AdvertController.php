af77870cc694906f2d5f55cce16dafe784575016
Raphael Liere <raphael.liere@hotmail.fr>
2021-04-28 14:37:34 +0200

    Paginator setup Ok, modif on the homepage template for responsive

--- src/Controller/AdvertController.php
+++ src/Controller/AdvertController.php
@@ -12,7 +12,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 /**
  * Class AdvertController
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,8 @@
 namespace App\Controller;
 use App\Repository\AdvertRepository;
 use App\Repository\CategoryRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,14 +14,22 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
-     * @param CategoryRepository $categoryRepository
      * @param AdvertRepository $advertRepository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
      * @return Response
      */
-    public function index(AdvertRepository $advertRepository): Response
+    public function index(AdvertRepository $advertRepository, PaginatorInterface $paginator, Request $request): Response
     {
+        $data = $advertRepository->findAll();
+        $adverts = $paginator->paginate(
+            $data,
+            $request->query->get('page', 1),
+            4
+        );
+
         return $this->render('home/index.html.twig', [
-            'adverts' => $advertRepository->findAll()
+            'adverts' => $adverts
         ]);
     }
 
